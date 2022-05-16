b8b4f06b2c4c876d7ff2c4aba781f1c9028fccc0
hasansin <63632399+hasansin@users.noreply.github.com>
2022-05-17 00:36:45 +0530

    new route added to admin.route.js

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -3,5 +3,5 @@ const { loginAdmin, getAdmin } = require("../controllers/admin.controller");
 
 router.get("/:id", getAdmin);
 router.post("/login", loginAdmin);
-
+router.get("/", (req, res) => res.status(200).send("<h3>SHOP API - ADMIN SERVICE</h3>"));
 module.exports = router;
